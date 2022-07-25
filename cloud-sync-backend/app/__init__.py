938a975c00a8df41f45095ab27bf0de1c266d78a
brokensythe <ademola.fabuyi@infinityworks.com>
2022-07-25 12:34:07 +0100

    added env variables

--- cloud-sync-backend/app/__init__.py
+++ cloud-sync-backend/app/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask
 from flask_cors import CORS
+from config import Config
 
 instance = Flask(__name__)
-cors = CORS(instance, origins="")
+instance.config.from_object(Config)
+cors = CORS(instance, origins=instance.config['FRONTEND_URL'])
 
 from app import routes
