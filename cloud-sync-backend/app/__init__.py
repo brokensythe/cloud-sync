503a21a5bd0fa76ba5db4b58b615f8a720ec690e
brokensythe <ademola.fabuyi@infinityworks.com>
2022-07-26 22:49:25 +0100

    added postgres DB container with working connectivity

--- cloud-sync-backend/app/__init__.py
+++ cloud-sync-backend/app/__init__.py
@@ -1,9 +1,13 @@
 from flask import Flask
 from flask_cors import CORS
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 instance = Flask(__name__)
 instance.config.from_object(Config)
 cors = CORS(instance, origins=instance.config['FRONTEND_URL'])
+db = SQLAlchemy(instance)
+migrate = Migrate(instance, db)
 
-from app import routes
+from app import routes, models
--- cloud-sync-backend/config.py
+++ cloud-sync-backend/config.py
@@ -6,3 +6,5 @@ load_dotenv(os.path.join(basedir, '.env'))
 
 class Config(object):
     FRONTEND_URL = os.environ.get('FRONTEND_URL')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
